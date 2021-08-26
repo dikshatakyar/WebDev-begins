ffb9e23df1829950885540d98dd7836795f33260
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2021-08-26 12:55:00 +0530

    alerts in button

---
+++ button.js
@@ -0,0 +1,12 @@
+// document.getElementById("another").onclick=function(){
+//     alert("Gangadhar hi shaktimaan hai");
+// }
+
+// let btn2=document.querySelector('[id="another"]');
+ let btn2=document.querySelector("#another");
+
+btn2.addEventListener('click',alertmsg);
+
+function alertmsg(){
+    alert("Gangadhar hi shaktimaan hai");
+}
